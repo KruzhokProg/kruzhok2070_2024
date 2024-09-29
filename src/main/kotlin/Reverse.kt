a0c2b4f6f45df1befd5b26b8582c6e7958bb0abe
Nikolay Irtuganov <vongu3@gmail.com>
2024-09-29 15:32:29 +0300

    разложение в ряд Тейлора

--- src/main/kotlin/Reverse.kt
+++ src/main/kotlin/Reverse.kt
@@ -32,6 +32,7 @@ package org.example
 //}
 
 // разложение в ряд
+// 1/(1-x)
 //fun main() {
 //    var x = readln().toDouble()
 //    val n = 100
@@ -43,6 +44,7 @@ package org.example
 //    println(1/(1-x))
 //}
 
+// 1/(1-x)^2
 //fun main() {
 //    var x = readln().toDouble()
 //    val n = 100
@@ -54,15 +56,75 @@ package org.example
 //    println(1/Math.pow(1-x, 2.0))
 //}
 
+// 1/(1-x)^3
+//fun main() {
+//    var x = readln().toDouble()
+//    val n = 100
+//    var s = 0.0
+//    var koef = 1
+//    for (i in 0..n-1) { // i=0, 1, 2
+//        s += koef*Math.pow(x, i*1.0)
+//        koef = koef + (i+2) // 6 + 4 = 10
+//    }
+//    println(s)
+//    println(1/Math.pow(1-x, 3.0))
+//}
+// ДЗ*
+// (1+x)^0.5
+//fun main() {
+//    var x = readln().toDouble()
+//    val n = 10
+//    var s = 1.0 + 0.5*x - 1/8*Math.pow(x, 2.0)
+//    var chis = 3
+//    var znam = 2*4*6
+//    var sign = 1
+//    for (i in 3..n-1) { // i=0..n-1
+//        s += sign*(chis/znam)*Math.pow(x, i*1.0)
+//        var chisCur = chis + 2 // 7
+//        chis = chis*chisCur // 3*5*7
+//        var znamCur = znam + 2
+//        znam = znam*znamCur
+//        sign = -sign
+//    }
+//    println(Math.pow(1+x, 0.5))
+//    println(s)
+//}
+
+// Палиндром
+// 12321
+//fun main() {
+//    var x = readln().toInt() // 123
+//    var y = x
+//    var res = 0
+//    while (x != 0) {
+//        val last = x % 10 // 1
+//        x = x / 10 // 0
+//        res = res * 10 + last // 32*10 + 1=321
+//    }
+//    println(res == y)
+//}
+
+// 123321
+// брать каждый раз первую и последнюю цифру и проверять на равенство
 fun main() {
-    var x = readln().toDouble()
-    val n = 100
-    var s = 0.0
-    var koef = 1
-    for (i in 0..n-1) { // i=0, 1, 2
-        s += koef*Math.pow(x, i*1.0)
-        koef = koef + (i+2) // 6 + 4 = 10
+    var x = readln().toInt() // 123
+    var y = x
+    var count = 0.0
+    while (x != 0) {
+        x = x / 10
+        count++
     }
-    println(s)
-    println(1/Math.pow(1-x, 3.0))
+    // 123 % 100
+    while (y != 0) {
+        val first = (y / Math.pow(10.0, count - 1)).toInt()
+        val last = y % 10
+        y = y % Math.pow(10.0, count - 1).toInt()
+        count = count - 2
+        if (first != last) {
+            println("Не палиндром")
+            return
+        }
+        y = y / 10
+    }
+    println("Палиндром")
 }
